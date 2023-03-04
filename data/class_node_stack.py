42d404eebd902c9eafe6a3abcad43d8e29bb163e
Dmitry <k1791@mail.ru>
2023-03-04 22:45:11 +0500

    lesson step 2
    Signed-off-by: Dmitry <k1791@mail.ru>

---
+++ data/class_node_stack.py
@@ -0,0 +1,26 @@
+
+class Node():
+    """"Класс узла"""
+
+    def __init__(self, data, next_node):
+       self.data = data
+       self.next_node = next_node
+
+# class Stack():
+#      """"Класс СТЭКА"""
+#
+#     def __init__(self): #инициализируется одним атрибутом, хранящим ссылку на верхний (крайний в стэке) узел. Для пустого стэка этот атрибут будет содержать `None`.
+#         stack_id+=1
+#
+#         self.link = push() # kind
+#         # self.etag = self.channel.get ("etag") # etag
+#
+#
+#     def push (self):
+#         """Метод добавления данных в Stack"""
+#         self.channel_statistic = json.dumps(self.channel, indent=2, ensure_ascii=False)
+#         print(self.channel_statistic )
+#         return
+
+
+
