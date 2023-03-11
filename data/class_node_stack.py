ac344d22c5c8840c16c023bd32f76fcab8d1a7e9
Dmitry <k1791@mail.ru>
2023-03-11 15:39:48 +0500

    continue lesson2
    Signed-off-by: Dmitry <k1791@mail.ru>

--- data/class_node_stack.py
+++ data/class_node_stack.py
@@ -30,32 +30,3 @@ class Stack():
         else:
             raise EmptyStackException
 
-    # def __init__(
-    #         self):  # инициализируется одним атрибутом, хранящим ссылку на верхний (крайний в стэке) узел. Для пустого стэка этот атрибут будет содержать `None`.
-    #     self.top = None
-    #     # self.size = 0
-    #
-    # def isEmpty(self):
-    #     return self.top is None
-    #
-    # def push(self, data) -> None:
-    #     """Метод добавления данных в Stack"""
-    #     self.top = Node(data, self.top)
-    #     # self.size += 1
-    #
-    # # def top(self) -> str:
-    # #     """Метод возвращающий верхнее значениее Stack"""
-    # #
-    # #     if self.top:
-    # #         return self.top.element
-    # #     else:
-    # #         raise EmptyStackException
-    #
-    # def pop(self) -> str:
-    #     # Remove a value from the stack and return.
-    #     if self.isEmpty():
-    #         raise EmptyStackException
-    #     val = self.top.data
-    #     self.top = self.top.nextNode
-    #     # self.size -= 1
-    #     return val
